535fdf5bf9fbf2896ec70fcca4141b1bea03deb1
Tamerlan Bimzhanov <bimzhanovt.net@gmail.com>
2023-02-28 22:33:38 +0600

    Allow only lowercase letters in username

--- realtime_chat/constants.py
+++ realtime_chat/constants.py
@@ -1,7 +1,7 @@
 MIN_USERNAME_LENGTH = 3
 MAX_USERNAME_LENGTH = 25
 
-USERNAME_REGEXP = r'^[a-zA-Z_]\w*$'
+USERNAME_REGEXP = r'^[a-z_][a-z0-9_]*$'
 USERNAME_REGEXP_MESSAGE = 'Username must start with a letter or underscore and contain only letters, numbers, or underscores'
 
 PASSWORD_HASH_LENGTH = 128
